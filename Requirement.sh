6db25779a96b729196ab8d0becc8ca4051704fdc
rishi <ghoshs4@lsbu.ac.uk>
2019-04-26 19:28:43 +0100

    pre_req.sh   [added]

--- Requirement.sh
+++ Requirement.sh
@@ -1,4 +1,8 @@
 # installation prerquisits
 
-sudo apt -y install python3 python3-pip
+sudo apt -y install python3 python3-pip figlet
+
+figlet 'ShellMon'
+echo 'press any key to continue...'
+read
 sudo python3 -m pip install numpy matplotlib drawnow psutil
