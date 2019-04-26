9463992a87da4e1aacefddeb8cd0441385e389fe
rishi <ghoshs4@lsbu.ac.uk>
2019-04-26 19:26:56 +0100

    pre_req.sh   [added]

--- Requirement.sh
+++ Requirement.sh
@@ -1,4 +1,4 @@
 # installation prerquisits
 
 sudo apt -y install python3 python3-pip
-python3 -m install numpy matplotlib drawnow psutil
+sudo python3 -m pip install numpy matplotlib drawnow psutil
