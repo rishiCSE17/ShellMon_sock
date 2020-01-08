4523eb5849275e684f4c4c02dd8559ba5606f460
Saptarshi Ghosh <46545669+rishiCSE17@users.noreply.github.com>
2020-01-08 04:36:21 +0000

    Update Requirement.sh

--- Requirement.sh
+++ Requirement.sh
@@ -1,9 +1,9 @@
 # installation prerquisits
 
-sudo apt update
-sudo apt install software-properties-common
+sudo apt -y update
+sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7 python3.7-dev
+sudo apt -y install python3.7 python3.7-dev
 
 sudo apt -y install python3 python3-pip figlet
 sudo python3.7 -m pip install --upgrade pip
