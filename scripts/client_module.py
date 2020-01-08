a876a9dbb387f71a41ea4a3d37b0f027ea0c5d8a
rishiCSE17 <ghoshs4@lsbu.ac.uk>
2020-01-08 06:46:20 +0000

    adds hadoop integration

--- scripts/client_module.py
+++ scripts/client_module.py
@@ -5,20 +5,55 @@ sends data to the server
 test
 '''
 
-
+import happybase as hb
 import socket
 import time
 import random
 import fetch_util as util
 import json
 
-def send_loop(client_port, host_ip, host_port, buffer_size=1024, cpu_interval=2, delay=1, mypass='', intf='eth0', is_wl=True):
+def hbase_conn(host, tab):
+    conn=hb.Connection(host)
+    return conn.table(tab)
+
+def bytefy(d):
+    ret={'cpu:vcore':str.encode(str(d['cpu']['count_vir'])),
+         'cpu:pcore':str.encode(str(d['cpu']['count_phy'])),
+         'cpu:freq':str.encode(str(d['cpu']['mean_freq'])),
+         'cpu:load':str.encode(str(d['cpu']['mean_load'])),
+         'mem:freq':str.encode(str(d['mem']['freq'])),
+         'mem:vol':str.encode(str(d['mem']['vol'])),
+         'mem:util':str.encode(str(d['mem']['util'])),
+         'net:ing':str.encode(str(d['net']['Ingress_util'])),
+         'net:egg':str.encode(str(d['net']['Egress_util'])),
+         'net:speed':str.encode(str(d['net']['Speed']))}
+    return ret
+
+def insert_hbase(tab, data):
+    tab.put(str(int(time.time())).encode(),bytefy(data))
+
+def send_loop(client_port,
+              host_ip,
+              host_port,
+              buffer_size=1024,
+              cpu_interval=2,
+              delay=1,
+              mypass='',
+              intf='eth0',
+              is_wl=True,
+              hbase=False,
+              hbase_ip='',
+              hbase_tab=''):
     print('================= TRANSMISSION INITIATED ==============')
 
     #initiate socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('0.0.0.0',client_port))
     s.connect((host_ip, host_port))
+
+    if hbase:
+        print(f'Connecting {hbase_ip}...')
+        htab = hbase_conn(hbase_ip, hbase_tab)
 
     while True:
         try:
@@ -31,6 +66,10 @@ def send_loop(client_port, host_ip, host_port, buffer_size=1024, cpu_interval=2,
                                      is_wl=is_wl)
 
             time.sleep(3)
+
+            if hbase:
+                insert_hbase(tab=htab,data=msg)
+
             msg_byte=str.encode(json.dumps(msg))
             s.send(msg_byte)
             print(f'| sent     \t | {msg_byte} \t |')
@@ -47,6 +86,15 @@ def main():
     buffer_size = int(input('Enter Buffer size \t : '))
     client_password=input('Enter your password : ')
     intf=input('Enter interface to monitor \t : ')
+    hbase=input('Using HBase Integration (y/n)? ')
+
+    if hbase == 'y':
+        hbase = True
+        hbase_ip = input('Enter HBase Server IP \t : ')
+        hbase_tab = input('Enter Target Table name \t : ')
+    else:
+        hbase = False
+
     while True:
         is_wl=input('is this a wireless interface ? (y/n) : \t')
         if is_wl == 'y':
@@ -63,6 +111,9 @@ def main():
               host_port=host_port,
               mypass=client_password,
               intf=intf,
-              is_wl=is_wl)
+              is_wl=is_wl,
+              hbase=hbase,
+              hbase_ip=hbase_ip,
+              hbase_tab=hbase_tab)
 
 main()
--- scripts/fetch_util.py
+++ scripts/fetch_util.py
@@ -11,7 +11,7 @@ def get_cpu_util(interval=None):
     dict_cpu['count_phy']=ps.cpu_count(logical=False)
     dict_cpu['count_vir']=ps.cpu_count()
     dict_cpu['mean_freq']=sum([x[0] for x in ps.cpu_freq(percpu=True)])/4
-    x = os.getloadavg()
+    x = ps.getloadavg()
     dict_cpu['mean_load']=round((15*x[0]+5*x[1]+x[2])/21,4)
 
     return dict_cpu
@@ -57,6 +57,25 @@ def main_loop(loop=True, delay=0, cpu_interval=None, my_pass='', intf='eth0',is_
                            is_wl=is_wl,
                            interval=1,
                            window=60)
+
+        '''
+        *** Datagram ***
+        {
+            cpu : { count_phy : val,
+                    count_vir : val,
+                    mean_frq  : val,
+                    mean_load : val
+                  },
+            mem : { vol     : val,
+                    util    : val,
+                    freq    : val  
+                  },
+            net : { Ingress_util : val,
+                    Egress_util  : val,
+                    speed        : val
+                  }
+        }                 
+        '''
 
         ##test block
         #send_data(datagram)
