541a8a4d8e21ce80ed35f3e9a295eb72918d5fc2
Saptarshi Ghosh <46545669+rishiCSE17@users.noreply.github.com>
2020-01-08 04:30:23 +0000

    Update Requirement.sh

--- Requirement.sh
+++ Requirement.sh
@@ -1,8 +1,14 @@
 # installation prerquisits
 
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.7
+
 sudo apt -y install python3 python3-pip figlet
+sudo python3.7 -m pip install --upgrade pip
 
 figlet 'ShellMon-Sock by Saptarshi'
 echo 'press any key to continue...'
-read
+read x
 sudo python3 -m pip install numpy matplotlib drawnow psutil
